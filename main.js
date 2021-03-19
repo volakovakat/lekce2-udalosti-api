675bb678351741638dc3113be378aa0b1217b828
kaczenka <75174671+kaczenka@users.noreply.github.com>
2021-03-19 18:57:28 +0100

    Řešení úkolu - události, API prvky

--- main.js
+++ main.js
@@ -1,1 +1,31 @@
 // tady je místo pro náš program
+let odstavec = document.querySelector("p");
+let pismo = 16;
+let zvuk = document.querySelector("#zvuk");
+
+function priNajeti() {
+    odstavec.classList.toggle("odstavec");
+
+}
+
+function cervenyPoKliknuti() {
+    odstavec.classList.toggle("cerveneTlacitko");
+}
+
+function vetsiPoKliknuti() {
+    pismo = pismo + 1;
+    odstavec.style.fontSize = pismo + 'px';
+}
+
+function prehrat() {
+    zvuk.play();
+}
+
+function hlasitost(uroven) {
+    zvuk.volume = uroven;
+}
+
+function odZacatku() {
+    zvuk.currentTime = 0;
+}
+
